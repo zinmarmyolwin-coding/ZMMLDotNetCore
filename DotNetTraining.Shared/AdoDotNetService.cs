df88e011e5dd2ae4f83f027fbdf519a581468726
Zin Mar Myo Lwin <zinmarmyolwin@acedatasystems.com>
2024-05-22 01:14:54 +0630

    Add Shared Project

---
+++ DotNetTraining.Shared/AdoDotNetService.cs
@@ -0,0 +1,91 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using Newtonsoft.Json;
+using DotNetTraining.Shared.Model;
+
+namespace DotNetTrainingBatch4.Shared
+{
+    public class AdoDotNetService
+    {
+        private readonly string _connectionString;
+
+        public AdoDotNetService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<T> Query<T>(string query, params AdoDotNetParameterModel[]? parameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            if (parameters is not null && parameters.Length > 0)
+            {
+                var parametersArray = parameters.Select(item => new SqlParameter(item.Name, item.Value)).ToArray();
+                cmd.Parameters.AddRange(parametersArray);
+            }
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            string json = JsonConvert.SerializeObject(dt);
+            List<T> lst = JsonConvert.DeserializeObject<List<T>>(json)!; 
+
+            return lst;
+        }
+
+        public T QueryFirstOrDefault<T>(string query, params AdoDotNetParameterModel[]? parameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            if (parameters is not null && parameters.Length > 0)
+            {
+                var parametersArray = parameters.Select(item => new SqlParameter(item.Name, item.Value)).ToArray();
+                cmd.Parameters.AddRange(parametersArray);
+            }
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            string json = JsonConvert.SerializeObject(dt); 
+            List<T> lst = JsonConvert.DeserializeObject<List<T>>(json)!; 
+
+            return lst[0];
+        }
+
+        public int Execute(string query, params AdoDotNetParameterModel[]? parameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            if (parameters is not null && parameters.Length > 0)
+            {
+                cmd.Parameters.AddRange(parameters.Select(item => new SqlParameter(item.Name, item.Value)).ToArray());
+            }
+
+            var result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+            return result;
+        }
+    }
+
+    public static class AdoDotNetParameterListExtension
+    {
+        public static List<AdoDotNetParameterModel> Add(this List<AdoDotNetParameterModel> lst, string name, object value)
+        {
+            lst.Add(new AdoDotNetParameterModel(name, value));
+            return lst;
+        }
+    }
+}
---
+++ DotNetTraining.Shared/Model/AdoDotNetParameterModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTraining.Shared.Model
+{
+    public class AdoDotNetParameterModel
+    {
+        public AdoDotNetParameterModel()
+        {
+        }
+
+        public AdoDotNetParameterModel(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+}
