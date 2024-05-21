d59fae863101b7d3526131097367a1d287c3f066
Zin Mar Myo Lwin <zinmarmyolwin@acedatasystems.com>
2024-05-21 23:28:41 +0630

    Add FormBlog

---
+++ DotNetTraining.WinFormsApp/FrmBlog.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace DotNetTraining.WinFormsApp
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnClick = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // btnClick
+            // 
+            btnClick.Location = new Point(385, 231);
+            btnClick.Name = "btnClick";
+            btnClick.Size = new Size(75, 23);
+            btnClick.TabIndex = 0;
+            btnClick.Text = "Click";
+            btnClick.UseVisualStyleBackColor = true;
+            btnClick.Click += btnClick_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(230, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 18);
+            label1.TabIndex = 1;
+            label1.Text = "Title :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(217, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 18);
+            label2.TabIndex = 2;
+            label2.Text = "Author :";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(217, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 18);
+            label3.TabIndex = 3;
+            label3.Text = "Content :";
+            label3.TextAlign = ContentAlignment.BottomLeft;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(319, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(202, 25);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(319, 77);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(202, 25);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(319, 120);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(202, 69);
+            textBox3.TabIndex = 6;
+            // 
+            // Test
+            // 
+            AutoScaleDimensions = new SizeF(6F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnClick);
+            ForeColor = SystemColors.ActiveCaptionText;
+            Name = "Test";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Blog";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnClick;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
---
+++ DotNetTraining.WinFormsApp/FrmBlog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetTraining.WinFormsApp
+{
+    public partial class Test : Form
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+    }
+}
